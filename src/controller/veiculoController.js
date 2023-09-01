eb5b7bfe248cd3260300f939bc4752770a5a14c6
Bruno O <brunodeoliveira.22.10@gmail.com>
2023-09-01 08:38:56 -0300

    novos endpoints

--- src/controller/veiculoController.js
+++ src/controller/veiculoController.js
@@ -1,4 +1,4 @@
-import { inserir, consultar } from '../repository/veiculoRepository.js'
+import { inserir, consultar, alterar, deletar } from '../repository/veiculoRepository.js'
 import { buscarTipoPorId, listarTipos } from '../repository/tipoVeiculoRepository.js'
 import { Router } from "express";
 
@@ -36,6 +36,72 @@ endpoints.post('/veiculo', async (req, resp) => {
   
 
 })
+
+
+
+
+
+
+endpoints.put('/veiculo/:id', async (req, resp) => {
+  try {
+    let veiculo = req.body;
+    let id = req.params.id;
+
+    if (!veiculo.modelo)
+      throw new Error('Modelo obrigatório');
+
+    if (!veiculo.ano || isNaN(veiculo.ano))
+      throw new Error('Ano deve ser um número')
+
+    
+    
+    // let r1 = await consultar(veiculo.placa);
+    // if (r1.length > 0)
+    //   throw new Error('Placa já cadastrada!');
+
+    
+    let r2 = await buscarTipoPorId(veiculo.idTipoVeiculo);
+    if (r2.length == 0)
+      throw new Error('Tipo inválido');
+
+
+    let r = await alterar(id, veiculo);
+    resp.send();
+  }
+  catch (err) {
+    resp.status(500).send({ erro: err.message });
+  }
+  
+
+})
+
+
+
+
+
+
+endpoints.delete('/veiculo/:id', async (req, resp) => {
+  try {
+    let id = req.params.id;
+    let r = await deletar(id);
+    if (r == 0)
+      throw new Error('Nenhum veículo pode ser excluído.');
+
+    resp.send();
+  }
+  catch (err) {
+    resp.status(500).send({ erro: err.message });
+  }
+});
+
+
+
+
+
+
+
+
+
 
 
 endpoints.get('/veiculo/tipo', async (req, resp) => {
